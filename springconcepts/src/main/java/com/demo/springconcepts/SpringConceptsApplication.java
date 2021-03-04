b4077d464714251d4b6623b9b6616e03d81e0e57
Komal-8080 <shindeg163@gmail.com>
2021-03-04 19:50:03 +0530

    ADDED: Welcome Message

---
+++ springconcepts/src/main/java/com/demo/springconcepts/SpringConceptsApplication.java
@@ -0,0 +1,14 @@
+package com.demo.springconcepts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConceptsApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome To Spring Concepts");
+		SpringApplication.run(SpringConceptsApplication.class, args);
+	}
+
+}
