e03ca2614dd4e6c8356a4e3bcdff5894c73ac2ec
Komal-8080 <shindeg163@gmail.com>
2021-03-05 23:38:37 +0530

    ADDED: To Respond Hello Messages to the user using Get Request Mathod and Rest Controller

--- SpringHelloWorld/src/main/java/com/example/helloWorld/controller/HelloRestController.java
+++ SpringHelloWorld/src/main/java/com/example/helloWorld/controller/HelloRestController.java
@@ -7,8 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello")
 public class HelloRestController {
 
+    //curl localhost:8080/hello -w "\n"
     @RequestMapping(value = {"","/","/home"})
     public String sayHello() {
-        return "Hello From Komal";
+        return "Hello From Bridgelabz!!!";
     }
 }
