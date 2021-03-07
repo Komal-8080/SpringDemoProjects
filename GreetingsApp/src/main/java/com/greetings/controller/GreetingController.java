8eb8ce8933e9350e7ac7cf5de94cc3391e2eecca
Komal-8080 <shindeg163@gmail.com>
2021-03-08 01:40:28 +0530

    ADDED:Service Layer and Repository to store data in a file

--- GreetingsApp/src/main/java/com/greetings/controller/GreetingController.java
+++ GreetingsApp/src/main/java/com/greetings/controller/GreetingController.java
@@ -1,6 +1,9 @@
 package com.greetings.controller;
 
 import com.greetings.model.Greeting;
+import com.greetings.model.User;
+import com.greetings.service.IGreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,10 +17,14 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
+    @Autowired
+    private IGreetingService greetingService;
+
     //curl -X GET "http://localhost:8080/greeting"
     @GetMapping(value={"","/","/home"})
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name){
-        return new Greeting(counter.incrementAndGet(),
-                            String.format(template,name));
+        User user = new User();
+        user.setFirstName(name);
+        return greetingService.addGreeting(user);
     }
 }
--- GreetingsApp/src/main/java/com/greetings/model/Greeting.java
+++ GreetingsApp/src/main/java/com/greetings/model/Greeting.java
@@ -1,8 +1,21 @@
 package com.greetings.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+
+@Entity
+@Table(name="GREETINGS")
 public class Greeting {
+
+    @Id
     private final long id;
     private final String message;
+
+    public Greeting() {
+        id = 0;
+        message = " ";
+    }
 
     public Greeting(long id, String message) {
         this.id = id;
